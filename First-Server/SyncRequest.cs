1ee59eb574dade9f6002f24afa2d7d889a146572
Oleksandr Bukovskyi <oleksandrbu@tahometer.com>
2020-08-26 15:29:29 +0300

    Refactoring

--- First-Server/SyncRequest.cs
+++ First-Server/SyncRequest.cs
@@ -2,12 +2,12 @@ using System.Collections.Generic;
 
 namespace First_Server{
     public class SyncRequest : AbstructRequest, IRequest{
-        public static string[] links = {"/who", "/how", "/does", "/what"};
+        public static string[] endpoints = {"/who", "/how", "/does", "/what"};
         public List<(string, string)> Request(List<string> urls){
             List<(string, string)> words = new List<(string, string)>();
 
             for (int i = 0; i < 4; i++){
-                words.Add(GetRequest($"http://{urls[i]}:3030{links[i]}"));
+                words.Add(GetRequest($"http://{urls[i]}:3030{endpoints[i]}"));
             }
             
             return words;
